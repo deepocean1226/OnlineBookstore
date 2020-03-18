cf348536de931ff3ceb162edfbb25466c845e25e
2372237664 <2372237664@qq.com>
2020-03-18 10:59:52 +0800

    666

--- OnlineBookstore/Pages/Login/Login.cshtml.cs
+++ OnlineBookstore/Pages/Login/Login.cshtml.cs
@@ -14,14 +14,45 @@ namespace OnlineBookstore
         private readonly IUserService userService;
         public List<UserInfo> userInfos = new List<UserInfo>();
 
+        [BindProperty]
+        public UserInfo UserInfo { get; set; }
+        [BindProperty]
+        public bool Name { get; set; }
+        [BindProperty]
+        public bool Password { get; set; }
+        [BindProperty]
+        public bool Login_b { get; set; }
         public LoginModel(IUserService userService)
         {
             this.userService = userService;
+            Name = true;
+            Password = true;
+            Login_b = false;
         }
 
         public async void OnGet()
         {
             userInfos = await userService.GetAll();
+        }
+
+        public async Task OnPostAsync()
+        {
+            UserInfo user = userInfos.Find(x => x.UserName == UserInfo.UserName);
+            if(user==null)
+            {
+                Name = false;
+            }
+            else
+            {
+                if (user.Pwd == UserInfo.Pwd)
+                {
+                    Login_b = true;
+                }
+                else
+                {
+                    Password = false;
+                }
+            }
         }
     }
 }
