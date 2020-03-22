d9010d5c54d909f685e91ded91208bb40ac4c31a
yunfei gao <2649793491@qq.com>
2020-03-22 16:05:30 +0800

    添加了登录页面

--- OnlineBookstore/Models/User.cs
+++ OnlineBookstore/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace OnlineBookstore.Models
 {
@@ -11,7 +12,9 @@ namespace OnlineBookstore.Models
         }
 
         public int UserId { get; set; }
+        [Required(ErrorMessage = "请输入密码！")]
         public string Pwd { get; set; }
+        [Required(ErrorMessage = "请输入用户名！")]
         public string UserName { get; set; }
         public int? ShoppingCartNo { get; set; }
 
--- OnlineBookstore/Pages/SignOn/SignOn.cshtml.cs
+++ OnlineBookstore/Pages/SignOn/SignOn.cshtml.cs
@@ -4,14 +4,52 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OnlineBookstore.Models;
+using OnlineBookstore.Services;
 
 namespace OnlineBookstore
 {
-    public class ChangePasswdModel : PageModel
+    public class SignOnModel : PageModel
     {
-        public void OnGet()
+        private readonly IUserService _userService;
+        
+        public bool Ok { get; set; }
+
+        public bool retype;
+
+        [BindProperty]
+        public User _User { get; set; }
+
+        public SignOnModel(IUserService userService)
         {
 
+            _userService = userService;
         }
+
+        [HttpPost]
+        public async Task<IActionResult> OnPost()
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            if (!await _userService.Add(_User))
+            { 
+                retype = true;
+                return Page();
+            }
+            else
+            {
+                retype = false;
+                Ok = true;
+            }
+            return Page();
+        }
+
+
+
+
+
     }
 }
